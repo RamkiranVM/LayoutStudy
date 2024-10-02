fba95e370f3b03de61fd80a132a2dd0c6313a751
RamkiranVM <ramkiranofficial@gmail.com>
2024-10-02 11:02:22 +0530

    initial commit with react project and components

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to Our Website!</h1>
+      <p>Explore our services and actions.</p>
+    </div>
+  );
+};
+
+export default Home;
